623b73aa39c61a0d6f7e34f02575562c0e7e81c3
Dario Di Gulio <dario.digulio@grupoesfera.com.ar>
2023-12-24 00:15:47 -0300

    Estilos para home

--- core/app/view/index-view.php
+++ core/app/view/index-view.php
@@ -1,23 +1,15 @@
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="row">
-                <div class="col-md-6">
-                    <img src="logo.png" class="img-responsive" alt="logo">
-                </div>
-                <div class="col-md-6">
-                    <h2>Bienvenido a la Bolsa de Empleo</h2>
-                    <p class="lead">
-                        La Bolsa de Empleo funciona como nexo entre las instituciones y empleadores que tienen búsquedas
-                        laborales y los estudiantes de las carreras de Psicología, Musicoterapia y Terapia Ocupacional
-                        que conforman la población de nuestra Facultad. Cuenta con diversas ofertas laborales
-                        actualizadas y subidas en la página de la bolsa. Los CVs postulados son enviados periódicamente
-                        a las instituciones generando un canal de comunicación permanente entre los centros y la
-                        oficina.
-                        Desde 2018 publicamos más de 1500 ofertas laborales dirigidas a estudiantes de la Facultad.
-                    </p>
-                </div>
-            </div>
-        </div>
+<div class="index-container">
+    <img src="logo.png" class="index-logo" alt="logo">
+    <div class="index-text-container">
+        <h2>Bienvenido a la Bolsa de Empleo</h2>
+        <p class="index-text">
+            La Bolsa de Empleo funciona como nexo entre las instituciones y empleadores que tienen búsquedas
+            laborales y los estudiantes de las carreras de Psicología, Musicoterapia y Terapia Ocupacional
+            que conforman la población de nuestra Facultad. Cuenta con diversas ofertas laborales
+            actualizadas y subidas en la página de la bolsa. Los CVs postulados son enviados periódicamente
+            a las instituciones generando un canal de comunicación permanente entre los centros y la
+            oficina.
+            Desde 2018 publicamos más de 1500 ofertas laborales dirigidas a estudiantes de la Facultad.
+        </p>
     </div>
 </div>
--- core/controller/View.php
+++ core/controller/View.php
@@ -1,51 +1,34 @@
 <?php
 
+class View
+{
+    public static function load($view)
+    {
+        if (!isset($_GET['view'])) {
+            include "core/app/view/" . $view . "-view.php";
+        } else {
+            if (View::isValid()) {
+                include "core/app/view/" . $_GET['view'] . "-view.php";
+            } else {
+                View::Error("<b>404 NOT FOUND</b> View <b>" . $_GET['view'] . "</b> folder !! - <a href='https://www.psi.uba.ar/bienestar/index.php' target='_blank'>Help</a>");
+            }
+        }
+    }
 
-// 13 de Abril del 2014
-// View.php
-// @brief Una vista corresponde a cada componente visual dentro de un modulo.
+    public static function isValid()
+    {
+        $valid = false;
+        if (isset($_GET["view"])) {
+            if (file_exists($file = "core/app/view/" . $_GET['view'] . "-view.php")) {
+                $valid = true;
+            }
+        }
+        return $valid;
+    }
 
-class View {
-	/**
-	* @function load
-	* @brief la funcion load carga una vista correspondiente a un modulo
-	**/	
-	public static function load($view){
-		// Module::$module;
-		if(!isset($_GET['view'])){
-			include "core/app/view/".$view."-view.php";
-		}else{
-
-
-			if(View::isValid()){
-				include "core/app/view/".$_GET['view']."-view.php";				
-			}else{
-				View::Error("<b>404 NOT FOUND</b> View <b>".$_GET['view']."</b> folder !! - <a href='https://www.psi.uba.ar/bienestar/index.php' target='_blank'>Help</a>");
-			}
-		}
-	}
-
-
-	/**
-	* @function isValid
-	* @brief valida la existencia de una vista
-	**/	
-	public static function isValid(){
-		$valid=false;
-		if(isset($_GET["view"])){
-			if(file_exists($file = "core/app/view/".$_GET['view']."-view.php")){
-				$valid = true;
-			}
-		}
-		return $valid;
-	}
-
-	public static function Error($message){
-		print $message;
-	}
+    public static function Error($message)
+    {
+        print $message;
+    }
 
 }
-
-
-
-?>
