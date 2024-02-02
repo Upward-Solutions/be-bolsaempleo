96014744d5bd82db2911483b97fa50ccb635374e
Dario Di Gulio <dario.digulio@grupoesfera.com.ar>
2024-02-02 12:01:22 -0300

    Actualiza local run para que funcione en windows y linux

--- local-run.sh
+++ local-run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-docker-compose up
+# Verifica el sistema operativo
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Sistema operativo Linux
+    docker-compose up
+elif [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "cygwin" ]]; then
+    # Sistema operativo Windows
+    winpty docker-compose up
+else
+    # Otros sistemas operativos (no soportados)
+    echo "Sistema operativo no soportado"
+    exit 1
+fi
