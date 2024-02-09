1a51cf4d3bae4c9a9da1dfe21b1bbfdf96003f75
DarioDiGulio <dariodigulio@gmail.com>
2024-02-09 17:44:42 -0300

    Arregla local-run para que soporte versiones de docker-compose

--- local-run.sh
+++ local-run.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    docker-compose up
+docker_compose_installed=$(command -v docker-compose)
+
+if [[ -n "$docker_compose_installed" ]]; then
+    echo "Docker Compose está instalado en la ruta: $docker_compose_installed"
+    compose_version=$("$docker_compose_installed" --version | awk '{print $3}')
+    echo "La versión de Docker Compose instalada es: $compose_version"
+
+    if [[ "$OSTYPE" == "linux-gnu"* && $(echo "$compose_version" | cut -d '.' -f 1) -ge 2 ]]; then
+        echo "Se utilizará 'docker compose up' en Linux."
+        docker compose up
+    else
+        echo "Se utilizará 'docker-compose up'."
+        docker-compose up
+    fi
 elif [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "cygwin" ]]; then
+    echo "Docker Compose no está instalado."
+    echo "Ejecutando 'winpty docker-compose up'..."
     winpty docker-compose up
 else
-    echo "Sistema operativo no soportado"
+    echo "Docker Compose no está instalado o no se encuentra en la ruta."
+    echo "Por favor, instala Docker Compose y vuelve a intentarlo."
     exit 1
 fi
