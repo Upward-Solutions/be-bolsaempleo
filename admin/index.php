8c1c71d661d1392b7b2589869ac842184ba68749
DarioDiGulio <dariodigulio@gmail.com>
2024-02-09 23:34:37 -0300

    Actualiza index de admin para que tome la variable de entorno como debug

--- admin/index.php
+++ admin/index.php
@@ -2,18 +2,18 @@
 
 define("ROOT", dirname(__FILE__));
 
-$debug= false;
-if($debug){
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+$debug = getenv('DEBUG');
+if ($debug) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 }
 
 include "core/autoload.php";
 
 ob_start();
 session_start();
-Core::$root="";
+Core::$root = "";
 
 $lb = new Lb();
 $lb->start();
