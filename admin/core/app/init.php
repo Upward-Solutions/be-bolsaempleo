e73bcac7da7318edf88230a5a43c78e1e7664cdd
DarioDiGulio <dariodigulio@gmail.com>
2024-02-09 23:41:54 -0300

    Formatted

--- admin/core/app/init.php
+++ admin/core/app/init.php
@@ -1,14 +1,8 @@
 <?php
-
-
-/// en caso de que el parametro action este definido evitamos que se muestre
-/// el layout por defecto y ejecutamos el action sin mostrar nada de vista
-// print_r($_GET);
-if(!isset($_GET["action"])){
-//	Bootload::load("default");
-	Module::loadLayout("index");
-}else{
-	Action::load($_GET["action"]);
-}
-
-?>
+    /// en caso de que el parametro action este definido evitamos que se muestre
+    /// el layout por defecto y ejecutamos el action sin mostrar nada de vista
+    if (!isset($_GET["action"])) {
+        Module::loadLayout("index");
+    } else {
+        Action::load($_GET["action"]);
+    }
