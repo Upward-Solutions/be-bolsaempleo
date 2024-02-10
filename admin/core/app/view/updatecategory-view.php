b19c6a68551f9b13e1afb5ebf31a39ae48951f9f
DarioDiGulio <dariodigulio@gmail.com>
2024-02-09 23:21:04 -0300

    Formateo js de la vista para actualizar categorías

--- admin/core/app/view/updatecategory-view.php
+++ admin/core/app/view/updatecategory-view.php
@@ -1,13 +1,8 @@
 <?php
 
-if(count($_POST)>0){
-	$user = CategoryData::getById($_POST["user_id"]);
-	$user->name = $_POST["name"];
-	$user->update();
-print "<script>window.location='index.php?view=categories';</script>";
-
-
-}
-
-
-?>
+    if (count($_POST) > 0) {
+        $user = CategoryData::getById($_POST["user_id"]);
+        $user->name = $_POST["name"];
+        $user->update();
+        print "<script>window.location='index.php?view=categories';</script>";
+    }
