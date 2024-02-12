617b16512fd86ad131f0b275c7ce16031c23ca4f
DarioDiGulio <dariodigulio@gmail.com>
2024-02-11 22:55:11 -0300

    Agrega primeros test del layout del core

---
+++ test/e2e/cypress/e2e/layout.cy.js
@@ -0,0 +1,43 @@
+describe('Layout test', () => {
+    context('WEB', () => {
+        it('Título', () => {
+            cy.get('.layout-title').should('contain', 'Bienestar Estudiantil')
+        })
+
+        it('Menú', () => {
+            cy.get('[href="./?view=vacantes"]').should('contain', 'Vacantes')
+        })
+
+        it('Footer', () => {
+            cy.get('.layout-footer-text').should('contain', 'Realizado por')
+            cy.get('.layout-footer-text').should('contain', '2024')
+            cy.get('.layout-footer-text > a').should('contain', 'Bienestar Estudiantil')
+        })
+    })
+
+    context('MOBILE', () => {
+        it('Título', () => {
+            cy.get('.layout-title').should('contain', 'Bienestar Estudiantil')
+        })
+
+        it('Menú', () => {
+            cy.get('[href="./?view=vacantes"]').should('contain', 'Vacantes')
+        })
+
+        it('Footer', () => {
+            cy.get('.layout-footer-text').should('contain', 'Realizado por')
+            cy.get('.layout-footer-text').should('contain', '2024')
+            cy.get('.layout-footer-text > a').should('contain', 'Bienestar Estudiantil')
+        })
+
+        beforeEach(() => {
+            cy.viewport('iphone-xr')
+        })
+    })
+
+    beforeEach(() => {
+        cy.visit(URL_BASE)
+    })
+
+    const URL_BASE = 'http://localhost:8080'
+})
