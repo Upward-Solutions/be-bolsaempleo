2a81b0779213639e1c73fd7bb1fd6108cf97ebbd
DarioDiGulio <dariodigulio@gmail.com>
2024-02-14 15:40:36 -0300

    Actualiza archivo setup.sh con mejores explicaciones de cada paso

--- setup.sh
+++ setup.sh
@@ -1,7 +1,47 @@
 #!/bin/bash
 
 carpeta="uploads"
+total_steps=3
+current_step=0
 
+
+print_progress() {
+    local progress=$((100 * current_step / total_steps))
+    local completed
+    local remaining
+    local progress_bar
+    local empty_bar
+    completed=$((progress / 2))
+    remaining=$((50 - completed))
+    progress_bar=$(printf "%-${completed}s" | tr ' ' '=')
+    empty_bar=$(printf "%-${remaining}s")
+    echo -ne "[$progress_bar$empty_bar] $progress%\r"
+}
+
+echo 'Configurando el proyecto'
+
+echo '1) Borrando carpeta ./uploads'
+if [[ "$(uname)" == "Darwin" ]]; then
+    rm -rf "$carpeta"
+else
+    rm -r "$carpeta"
+fi
+((current_step++))
+print_progress
+echo
+
+echo '2) Creando nueva carpeta ./uploads'
 mkdir "$carpeta"
+((current_step++))
+print_progress
+echo
 
-chmod 700 "$carpeta"
+echo '3) Asignando permisos a la carpeta ./uploads'
+if [[ "$(uname)" != "Darwin" ]]; then
+    chmod -R 777 "$carpeta"
+fi
+((current_step++))
+print_progress
+echo
+
+echo 'DONE'
