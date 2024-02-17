b462a994505e120de570beb26979fe38cda98e8d
DarioDiGulio <dariodigulio@gmail.com>
2024-02-17 18:38:12 -0300

    Ordena autoloads por alfabeto

--- core/autoload.php
+++ core/autoload.php
@@ -1,16 +1,16 @@
 <?php
 
-include "controller/Core.php";
-include "controller/View.php";
-include "controller/Module.php";
 include "admin/core/controller/Database.php";
+include "domain/autoload.php";
+include "controller/Action.php";
+include "controller/Bootload.php";
+include "controller/class.upload.php";
+include "controller/Core.php";
+include "controller/Crudadmin.php";
 include "controller/Executor.php";
+include "controller/Form.php";
 include "controller/Lb.php";
 include "controller/Model.php";
-include "controller/Bootload.php";
-include "controller/Action.php";
-include "controller/class.upload.php";
-include "controller/Form.php";
-include "controller/Crudadmin.php";
+include "controller/Module.php";
+include "controller/View.php";
 include "vendor/autoload.php";
-include "domain/autoload.php";
--- domain/files/autoload.php
+++ domain/files/autoload.php
@@ -1,7 +1,7 @@
 <?php
 
-include "Files.php";
 include "FileData.php";
 include "FileNotFoundError.php";
+include "Files.php";
 include "storage/LocalStorage.php";
 include "storage/StorageConnection.php";
