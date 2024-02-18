600a4b674f9c2479f015c38cb9237353f176e8b5
DarioDiGulio <dariodigulio@gmail.com>
2024-02-18 12:42:31 -0300

    Reestaura el sistema de descarga local

--- admin/core/app/action/download-action.php
+++ admin/core/app/action/download-action.php
@@ -1,26 +1,34 @@
 <?php
-$filePath = '/var/www/html/uploads/' . $_GET['file_id'];
-echo $filePath;
 
-$originalContent = file_get_contents($filePath);
-if ($originalContent === false) {
-    Core::alert("Error al leer el contenido del archivo.");
+use JetBrains\PhpStorm\NoReturn;
+
+#[NoReturn] function localDownload(): void
+{
+    $filePath = '/var/www/html/uploads/' . $_GET['file_id'];
+    echo $filePath;
+
+    $originalContent = file_get_contents($filePath);
+    if ($originalContent === false) {
+        Core::alert("Error al leer el contenido del archivo.");
+        exit;
+    }
+
+    $fileExtension = pathinfo($filePath, PATHINFO_EXTENSION);
+    $contentType = match ($fileExtension) {
+        'pdf' => 'application/pdf',
+        'txt' => 'text/plain',
+        default => 'application/octet-stream',
+    };
+
+    if (file_exists($filePath)) {
+        header('Content-Type: ' . $contentType);
+        header('Content-Disposition: inline; filename="' . basename($filePath) . '"');
+        header('Content-Length: ' . filesize($filePath));
+        readfile($filePath);
+    } else {
+        echo "Archivo no encontrado";
+    }
     exit;
 }
 
-$fileExtension = pathinfo($filePath, PATHINFO_EXTENSION);
-$contentType = match ($fileExtension) {
-    'pdf' => 'application/pdf',
-    'txt' => 'text/plain',
-    default => 'application/octet-stream',
-};
-
-if (file_exists($filePath)) {
-    header('Content-Type: ' . $contentType);
-    header('Content-Disposition: inline; filename="' . basename($filePath) . '"');
-    header('Content-Length: ' . filesize($filePath));
-    readfile($filePath);
-} else {
-    echo "Archivo no encontrado";
-}
-exit;
+localDownload();
