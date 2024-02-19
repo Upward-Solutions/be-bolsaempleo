31c68358b2df471170af9bcceeedafd70d4d6953
DarioDiGulio <dariodigulio@gmail.com>
2024-02-19 13:52:10 -0300

    Agrega version del despliegue visible

--- admin/core/app/layouts/layout.php
+++ admin/core/app/layouts/layout.php
@@ -105,7 +105,7 @@ function userName(): string
         </div>
         <footer class="main-footer">
             <div class="pull-right hidden-xs">
-                <b>Version</b> v1.1
+                <b>Version</b> <span id="commit-hash">{commit_hash}</span>
             </div>
             <strong>Copyright &copy; 2023 <a href="https://www.psi.uba.ar/bienestar/index.php" target="_blank">Bienestar
                     Estudiantil</a></strong>
