80413a787c2e66305719a94ee63a7122e7f990ad
DarioDiGulio <dariodigulio@gmail.com>
2024-02-21 12:03:14 -0300

    Actualiza estilos de la vista index

--- core/app/view/index-view.php
+++ core/app/view/index-view.php
@@ -1,15 +1,13 @@
-<div class="index-container">
-    <img src="logo.png" class="index-logo" alt="logo">
-    <div class="index-text-container">
-        <h2>Bienvenido a la Bolsa de Empleo</h2>
-        <p class="index-text">
-            La Bolsa de Empleo funciona como nexo entre las instituciones y empleadores que tienen b&uacute;squedas
-            laborales y los estudiantes de las carreras de Psicología, Musicoterapia y Terapia Ocupacional
-            que conforman la poblaci&oacute;n de nuestra Facultad. Cuenta con diversas ofertas laborales
-            actualizadas y subidas en la p&aacute;gina de la bolsa. Los CVs postulados son enviados peri&oacute;dicamente
-            a las instituciones generando un canal de comunicaci&oacute;n permanente entre los centros y la
-            oficina.
-            Desde 2018 publicamos m&aacute;s de 1500 ofertas laborales dirigidas a estudiantes de la Facultad.
-        </p>
-    </div>
+<img src="logo.png" class="index-logo" alt="logo">
+<div class="index-text-container">
+    <h2>Bienvenido a la Bolsa de Empleo</h2>
+    <p class="index-text">
+        La Bolsa de Empleo funciona como nexo entre las instituciones y empleadores que tienen b&uacute;squedas
+        laborales y los estudiantes de las carreras de Psicología, Musicoterapia y Terapia Ocupacional
+        que conforman la poblaci&oacute;n de nuestra Facultad. Cuenta con diversas ofertas laborales
+        actualizadas y subidas en la p&aacute;gina de la bolsa. Los CVs postulados son enviados peri&oacute;dicamente
+        a las instituciones generando un canal de comunicaci&oacute;n permanente entre los centros y la
+        oficina.
+        Desde 2018 publicamos m&aacute;s de 1500 ofertas laborales dirigidas a estudiantes de la Facultad.
+    </p>
 </div>
